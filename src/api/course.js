cbfb0bed6d26b5f71b49fa4808c3f4bdce07845f
lyg <1247838700@qq.com>
2020-04-05 13:19:17 +0800

    改一下

--- src/api/course.js
+++ src/api/course.js
@@ -35,6 +35,10 @@ export default{
             method:'post',
             data:searchObj
         })
+    },
+    getCourseDetails(courseId){
+        return request({
+            url:'/'
+        })
     }
-
 }
